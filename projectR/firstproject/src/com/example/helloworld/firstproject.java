02ab01556a18b4836e60acdab75533e4e4d98263
kubra soykan <kubsoykan@gmail.com>
2018-05-02 18:03:17 +0300

    hello wrold java

---
+++ projectR/firstproject/src/com/example/helloworld/firstproject.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * HelloworldJava
+ */
+public class firstproject {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
