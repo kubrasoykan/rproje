e239eb3689cc6dfbefec5548b3c6c033a34ebabf
kubra soykan <kubsoykan@gmail.com>
2018-05-07 19:47:50 +0300

    gui deneme

---
+++ src/deenemegui.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+
+public class deenemegui {
+    private JTabbedPane tabbedPane1;
+    private JTextArea informationsAboutSoftwareTextArea;
+    private JButton createEdgesButton;
+    private JButton visualSettingsButton;
+    private JButton takeGirthsButton;
+    private JButton tableOfNodesAndButton;
+    private JButton takeComponentsButton;
+    private JButton createNodesButton;
+    private JButton networkModelButton;
+    private JButton saveTableButton;
+    private JButton subsetDataButton1;
+    private JButton loadDataButton1;
+    private JButton cleanDataButton1;
+    private JButton saveDataButton;
+    private JTextField FRAUDSMONITORINGSYSTEMFORTextField;
+    private JTable table3;
+    private JButton normalAccidentsButton;
+    private JButton dataAnomaliesButton;
+    private JButton thirdDegreeFraudAlertButton;
+    private JButton firstDegreeFraudAlertButton;
+    private JButton secondDegreeFraudAlertButton;
+    private JTable table2;
+    private JTable table1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
